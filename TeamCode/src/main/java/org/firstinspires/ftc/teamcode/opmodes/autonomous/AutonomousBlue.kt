65ed891495a8da72a309ab121024729c456b5d46
Julian B. <a01714453@.tec.mx>
2026-09-19 13:27:46 -0600

    Pedro Pathing was completely removed and now the base code compiles properly. By Julian B.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousBlue.kt
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.opmodes.autonomous
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import org.firstinspires.ftc.teamcode.AutonomousBuilder
-import org.firstinspires.ftc.teamcode.utils.Alliance
-
-@Autonomous(group = "Auto", name = "Auto - Blue")
-class AutonomousBlue: AutonomousBuilder(Alliance.BLUE)
