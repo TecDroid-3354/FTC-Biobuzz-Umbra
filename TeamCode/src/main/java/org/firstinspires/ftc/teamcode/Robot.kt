7d9821e3cc2ebd49db4ec45f3afdd96a8cd61828
Julian B. <a01714453@.tec.mx>
2026-09-19 13:38:22 -0600

    The Robot.kt was fixed since it did commit proprely. Julian B.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
@@ -8,12 +8,10 @@ import com.qualcomm.robotcore.hardware.HardwareMap
 import com.seattlesolvers.solverslib.command.Command
 import com.seattlesolvers.solverslib.gamepad.GamepadEx
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants
-import org.firstinspires.ftc.teamcode.subsystems.mecanumDrive.Mecanum
+
 import org.firstinspires.ftc.teamcode.utils.Alliance
 import org.firstinspires.ftc.teamcode.utils.TecDroidRobot
-import org.firstinspires.ftc.teamcode.utils.autonomous.PoseTracker
-import org.firstinspires.ftc.teamcode.utils.extensions.toPose
+
 
 class Robot(
     private val alliance: Alliance,
@@ -25,7 +23,6 @@ class Robot(
     /* Declare your Pedro Pathing's Follower here */
     private lateinit var follower: Follower
     /* Declare your subsystems here */
-    private lateinit var drive: Mecanum
 
     init {
         subsystemInitialization()
@@ -34,9 +31,8 @@ class Robot(
     /* Initialize your subsystems and follower here */
     override fun subsystemInitialization() {
         // Follower initialization
-        follower = Constants.createFollower(hardwareMap)
+
         // Subsystem initialization
-        drive = Mecanum(follower, controller, alliance)
     }
 
     /* Runs indefinitely after the init button on the DS is pressed. Stops when play button is pressed */
@@ -45,25 +41,24 @@ class Robot(
     /* Initialize your teleop controller commands here */
     override fun initTeleOp() {
         // Chassis default command
-        drive.defaultCommand = drive.driveFollowingDriverInput()
-        drive.setPose(PoseTracker.lastPose)
+
         // Build Commands:
         // controller.button().onTrue(Command)
     }
 
     /* Initialize your auto commands here, set chassis alliance and starting pose */
     override fun initAuto(startingPose: Pose) {
-        drive.setPose(startingPose)
+
     }
 
     /* When the teleop ends, declare what to do */
     override fun onEnd() {
-        PoseTracker.lastPose = follower.pose
+
     }
 
     /* Print telemetry using the pTelemetry object on RobotConstants.Telemetry. It will be printed on both Panels and Driver Hub */
     override fun printTelemetry() {
-        pTelemetry.addData("Robot Pose", drive.getPose2D())
+
     }
 
     /**
@@ -73,6 +68,6 @@ class Robot(
 
     /* Common method to follow any path */
     override fun followPathCMD(path: PathChain, holdEnd: Boolean, maxPower: Double): Command {
-        return drive.followPathCMD(path, holdEnd, maxPower)
+        return TODO() //Yet to be implemented
     }
 }
