ccfe4c52733aeb98b2b44c61c519c54090078f01
7pet4x-netizen <7pe4x@gmail.com>
2026-09-26 13:16:39 -0600

    Avance de el código de la catapulta. Ahora escribe Daniel. -Naomi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Subsystems/Catapult/CatapultConstants.kt
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.subsystems.Subsystems.Catapult
+
+import com.bylazar.configurables.annotations.Configurable
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.PIDCoefficients
+import com.seattlesolvers.solverslib.controller.wpilibcontroller.SimpleMotorFeedforward
+import com.seattlesolvers.solverslib.hardware.motors.Motor
+
+object CatapultConstants {
+
+    object Identification {
+        val rightMotorID = "Motor1"
+        val leftMotorID = "Motor2"
+    }
+
+    object Configuration {
+        val runMode = Motor.RunMode.PositionControl
+        val zeroPowerBehavior = Motor.ZeroPowerBehavior.FLOAT
+        val rightInverted = true
+        val leftInverted = false
+    }
+
+    @Configurable
+    object Tunables {
+        @JvmField
+        var pidCoefficients = PIDCoefficients(0.0, 0.0, 0.0)
+
+        @JvmField
+        var feedforward = SimpleMotorFeedforward(0.0, 0.0)
+    }
+}
